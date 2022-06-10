4a5955c3b1e976a242bc1b590f722cc40ac42d41
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-06-10 18:22:09 -0400

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def match(arrAnagrams)
+        found = arrAnagrams.find_all { |anagram| anagram.chars.sort == word.chars.sort }
+        found ? found : []
+    end
+end
